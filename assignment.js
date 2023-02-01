6b766002763ed806001db6b34f7bd2b920353448
Hussein <aisackhussein@gmail.com>
2023-02-01 17:23:48 +0300

    Added array methods

--- assignment.js
+++ assignment.js
@@ -323,3 +323,38 @@ console.log(age1, age2, age3, age4)
  const newAges = [getAges(ages[0]), getAges(ages[1]), getAges(ages[2]), getAges(ages[ages.length - 1])];
  console.log(newAges)
 
+
+// basic array operations (methods)
+// push method 
+// push returns the length of the new array
+// add elements to an array 
+
+// code challenge 
+function calctTip(amount){
+    if (amount >= 50 && amount <= 300){
+        return amount * 0.15;
+    }else{
+        return amount * 0.2
+    }
+}
+
+console.log(calctTip(400))
+
+
+const bills = [125, 555, 44]
+
+// using an arrow function
+const calcTip = amount => {
+    if (amount >= 50 && amount <= 300){
+        return amount * 0.15;
+    }else{
+        return amount * 0.2
+    }
+}
+// console.log(calcTip(200))
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+const totalTips = [bills[0]+tips[0], bills[1]+tips[1], bills[2]+tips[2]]
+
+console.log(tips)
+console.log(totalTips)
