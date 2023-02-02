15342f0790089d701df51dc5fbd19ae990923bd8
Hussein <aisackhussein@gmail.com>
2023-02-02 16:10:11 +0300

    Finished the end of fundamentals 2 coding challenge

--- assignment.js
+++ assignment.js
@@ -534,4 +534,33 @@ while(dice !== 6){
 }
 
 
+// fundamentals two coding challenge
+
+function calcTip(bill){
+    return bill >=50 && bill <=300 ? bill * 0.15 : bill * 0.2;
+}
+const billls = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]
+const tipss = []
+const total = []
+
+for (i = 0; i < bills.length; i++){
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    total.push(tip + bills[i]);
+}
+
+console.log(bills)
+console.log(tips)
+console.log(total)
+
+
+const calcAverage  = function (arr){
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++){
+        sum += arr[i];
+    }
+    return sum / arr.length
+}
+console.log(calcAverage([6, 12, 9]))
+
 
