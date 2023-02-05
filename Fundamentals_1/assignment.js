b901141677199f3ccc14e3d0398544766b33db1c
Hussein <aisackhussein@gmail.com>
2023-02-05 16:42:30 +0300

    Added projects

--- Fundamentals_1/assignment.js
+++ Fundamentals_1/assignment.js
@@ -578,9 +578,9 @@ while (dice !== 6) {
 
 // fundamentals two coding challenge
 
-function calcTip(bill) {
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-}
+// function calcTip(bill) {
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// }
 const billls = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const tipss = [];
 const total = [];
