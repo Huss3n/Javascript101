1ec2493bf5196d1f5a7ad605f13df7f7f4407622
Hussein <aisackhussein@gmail.com>
2023-02-06 12:40:09 +0300

    Classes

--- Project/Modal/app.js
+++ Project/Modal/app.js
@@ -1,1 +1,9 @@
 "use strict";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const closeModalBtn = document.querySelector(".close-modal");
+const openModalBtn = document.querySelectorAll(".show-modal");
+
+for (let i = 0; i <= openModalBtn.length; i++) {
+  console.log(openModalBtn[i].textContent);
+}
