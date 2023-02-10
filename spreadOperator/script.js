56daf71fd9919b48162f7d8675337e9d505e0b08
Hussein <aisackhussein@gmail.com>
2023-02-10 20:45:14 +0300

    Fixed unexpected token error

--- spreadOperator/script.js
+++ spreadOperator/script.js
@@ -7,4 +7,46 @@ const newarr = [12, 14, arr[0], arr[1], arr[2]];
 console.log(newarr);
 
 const brandNew = [12, 45, 60, ...arr];
-console.log(brandNew);
+// logging individual elements
+console.log(...brandNew);
+
+const restaurant = {
+  name: "pizzaHouse",
+  location: "inYou",
+  menu: [
+    "periperi",
+    "hawain",
+    "vegi",
+    "gnocci",
+    "lasagna",
+    "fruit pie",
+    "caramel",
+  ],
+  drinks: ["pepsi", "coke", "getorade", "water"],
+  order: function (indexAt) {
+    return `order of ${indexAt} has been received`;
+  },
+};
+
+// builds a new array from scratch by expanding the new array
+// helps getting elements out of the array
+// can only be used in places where we are writing values separated by commas
+
+const newMenu = [...restaurant.menu, "Rice"];
+console.log(newMenu);
+
+// copying the array
+const copyArray = [...newMenu];
+console.log(copyArray);
+
+// joining two arrays
+const joinedArray = [...restaurant.menu, ...restaurant.drinks];
+console.log(joinedArray);
+
+// spread operator works on all iterables i.e strings, arrays, maps and sets but not objects
+
+// using it on a string
+const firstName = "hussein";
+const letters = [...firstName, "", "S"];
+console.log(letters);
+console.log(...letters);
