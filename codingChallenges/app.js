1d2c455d9c8340595a0c9b737c02e5f7d3c6149c
Hussein <aisackhussein@gmail.com>
2023-02-15 11:14:59 +0300

    Code challenge

--- codingChallenges/app.js
+++ codingChallenges/app.js
@@ -126,3 +126,90 @@ for ([min, event] of gameEvents) {
   const half = min <= 45 ? "First" : "Second";
   console.log(`[${half} Half] ${min}: ${event}`);
 }
+
+// check string
+const middleSeat = function (seat) {
+  // B and E are the middle seats
+  console.log(seat.slice(-1) === "B" ? "Middle seat" : "edge seat");
+};
+
+middleSeat("1h");
+
+const announcement =
+  "All passengers please come to boarding door 23. Boarding door 23";
+console.log(announcement.replaceAll("door", "gate"));
+
+// working with strings
+const newName = "Hussein Aisak";
+// add the two names in an array
+const [first, last] = [newName.split(" ")];
+
+const joined = ["mr.", first, last].join("");
+console.log(joined);
+
+const passenger = "jessica";
+
+const capitalizeName = function (name) {
+  const names = name.split(" ");
+  const namesUpper = [];
+  for (const n of names) {
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(" "));
+};
+capitalizeName("jessica ann smith davis");
+capitalizeName("hussein aisak");
+
+const message = "Go to gate 23";
+console.log(message.padStart(20, "+").padEnd(30, "+"));
+console.log(passenger.padStart(12, "+").padEnd(20, "+"));
+
+// mask credit card
+const maskCreditCard = (card) => {
+  const converted = card + "";
+  const last = converted.slice(-4);
+  return last.padStart(converted.length, "*");
+};
+
+console.log(maskCreditCard(23465782837472));
+console.log(maskCreditCard("476266127632647"));
+
+// repeat method
+const repeatMessage = "Bad weather, flights have been cancelled";
+console.log(repeatMessage.repeat(10));
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+Afterwards, test with your own test data!
+GOOD LUCK 😀
+*/
+
+console.log("<------Coding challenge------->");
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
+document.querySelector("button").addEventListener("click", function () {
+  const text = document.querySelector("textarea").value;
+  const rows = text.split("\n");
+  for (const [i, row] of rows.entries()) {
+    const [first, last] = row.toLowerCase().trim().split("_");
+    const output = `${first}${last.replace(last[0], last[0].toUpperCase())}`;
+    console.log(`${output.padEnd(20)}${"✅".repeat(i + 1)}`);
+  }
+});
