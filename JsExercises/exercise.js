dd76a994bfd3026ac3bb21f9572fdb9c1e16ee29
Hussein <aisackhussein@gmail.com>
2023-03-14 11:31:23 +0300

    Check prime number

--- JsExercises/exercise.js
+++ JsExercises/exercise.js
@@ -80,3 +80,26 @@ function factorial(n) {
 }
 
 console.log(factorial(5));
+
+// check prime number
+function checkPrime(num, div = 2) {
+  // Base case
+  if (num <= 2) {
+    return num == 2 ? true : false;
+  }
+
+  // No Divisor case
+  if (div * div > num) {
+    return true;
+  }
+
+  // Number is not prime if divisible
+  if (num % div == 0) {
+    return false;
+  }
+  // Recursive call with next divisor
+  return checkPrime(num, div + 1);
+}
+
+console.log(checkPrime(27)); // false
+console.log(checkPrime(19)); // true
