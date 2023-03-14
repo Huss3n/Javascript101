12a62be925d76c05a49e7d5882b06d9a4d600cc2
Hussein <aisackhussein@gmail.com>
2023-03-14 20:25:29 +0300

    period is late

--- JsExercises/exercise.js
+++ JsExercises/exercise.js
@@ -353,3 +353,22 @@ function binaryArrayToNumber(arr) {
     return acc + curr * Math.pow(2, arr.length - index - 1);
   }, 0);
 }
+
+/*
+In this kata, we will make a function to test whether a period is late.
+
+Our function will take three parameters:
+
+last - The Date object with the date of the last period
+
+today - The Date object with the date of the check
+
+cycleLength - Integer representing the length of the cycle in days
+
+Return true if the number of days passed from last to today is greater than cycleLength. Otherwise, return false. */
+
+function periodIsLate(last, today, cycleLength) {
+  const oneDay = 24 * 60 * 60 * 1000; // number of milliseconds in one day
+  const daysPassed = Math.round(Math.abs((last - today) / oneDay));
+  return daysPassed > cycleLength;
+}
