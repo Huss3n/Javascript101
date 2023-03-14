4d5395447bc008073bde9d01b45f217a6bc92ebb
Hussein <aisackhussein@gmail.com>
2023-03-14 22:35:44 +0300

    sum of divided

--- JsExercises/exercise.js
+++ JsExercises/exercise.js
@@ -833,3 +833,68 @@ function validParentheses(parens) {
   }
   return stack.length === 0;
 }
+
+/*
+DESCRIPTION:
+Given an array of positive or negative integers
+
+ I= [i1,..,in]
+
+you have to produce a sorted array P of the form
+
+[ [p, sum of all ij of I for which p is a prime factor (p positive) of ij] ...]
+
+P will be sorted by increasing order of the prime numbers. The final result has to be given as a string in Java, C#, C, C++ and as an array of arrays in other languages.
+
+Example:
+I = [12, 15]; //result = [[2, 12], [3, 27], [5, 15]]
+[2, 3, 5] is the list of all prime factors of the elements of I, hence the result.
+
+Notes:
+
+It can happen that a sum is 0 if some numbers are negative!
+Example: I = [15, 30, -45] 5 divides 15, 30 and (-45) so 5 appears in the result, the sum of the numbers for which 5 is a factor is 0 so we have [5, 0] in the result amongst others.
+
+In Fortran - as in any other language - the returned string is not permitted to contain any redundant trailing whitespace: you can use dynamically allocated character strings. */
+function sumOfDivided(lst) {
+  let primes = {};
+
+  // A helper function to check if a number is prime
+  function isPrime(n) {
+    if (n < 2) {
+      return false;
+    }
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // Loop through each number in the input array
+  for (let i = 0; i < lst.length; i++) {
+    let num = lst[i];
+    let absNum = Math.abs(num);
+
+    // Loop through each possible prime factor of the number
+    for (let j = 2; j <= absNum; j++) {
+      if (isPrime(j) && absNum % j === 0) {
+        // If the factor is prime and divides the number, add it to the primes object
+        if (!primes[j]) {
+          primes[j] = 0;
+        }
+        primes[j] += num;
+      }
+    }
+  }
+
+  // Convert the primes object to an array and sort by prime number
+  let result = [];
+  for (let prime in primes) {
+    result.push([parseInt(prime), primes[prime]]);
+  }
+  result.sort((a, b) => a[0] - b[0]);
+
+  return result;
+}
