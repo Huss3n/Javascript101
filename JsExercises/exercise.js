93b3895b19b4d5571a5c915390c6ba70f953c917
Hussein <aisackhussein@gmail.com>
2023-03-14 22:23:56 +0300

    rot13

--- JsExercises/exercise.js
+++ JsExercises/exercise.js
@@ -655,3 +655,22 @@ function int32ToIp(int32) {
   // Join the decimal values with dots to form the IPv4 address string
   return decimals.join(".");
 }
+
+/*
+ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+
+Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation". */
+function rot13(message) {
+  let ciphered = "";
+  for (let i = 0; i < message.length; i++) {
+    let char = message[i];
+    if (char >= "a" && char <= "z") {
+      ciphered += String.fromCharCode(((char.charCodeAt(0) - "a".charCodeAt(0) + 13) % 26) + "a".charCodeAt(0));
+    } else if (char >= "A" && char <= "Z") {
+      ciphered += String.fromCharCode(((char.charCodeAt(0) - "A".charCodeAt(0) + 13) % 26) + "A".charCodeAt(0));
+    } else {
+      ciphered += char;
+    }
+  }
+  return ciphered;
+}
