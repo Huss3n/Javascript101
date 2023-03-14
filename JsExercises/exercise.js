bf3bcaae4830d647b7818a4ef6214749dc6220e2
Hussein <aisackhussein@gmail.com>
2023-03-14 22:33:59 +0300

    valid paranthesis ()

--- JsExercises/exercise.js
+++ JsExercises/exercise.js
@@ -805,3 +805,31 @@ function getPINs(observed) {
 
   return variations;
 }
+
+/*
+DESCRIPTION:
+Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+
+Examples
+"()"              =>  true
+")(()))"          =>  false
+"("               =>  false
+"(())((()())())"  =>  true
+Constraints
+0 <= input.length <= 100 */
+function validParentheses(parens) {
+  let stack = [];
+  for (let i = 0; i < parens.length; i++) {
+    let char = parens[i];
+    if (char === "(") {
+      stack.push(char);
+    } else if (char === ")") {
+      if (stack.length === 0) {
+        return false;
+      } else {
+        stack.pop();
+      }
+    }
+  }
+  return stack.length === 0;
+}
