af0e4c73e2015c85713296f73fc06302d5d36f07
Hussein <aisackhussein@gmail.com>
2023-03-14 20:04:44 +0300

    aplhabet position

--- JsExercises/exercise.js
+++ JsExercises/exercise.js
@@ -263,3 +263,27 @@ function fridayTheThirteenths(startYear, endYear) {
   return dates.join(" ");
 }
 console.log(fridayTheThirteenths(1999, 2000));
+
+/*
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+Example
+alphabetPosition("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+ */
+function alphabetPosition(text) {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let result = "";
+
+  for (let i = 0; i < text.length; i++) {
+    let char = text[i].toLowerCase();
+    if (alphabet.indexOf(char) !== -1) {
+      result += alphabet.indexOf(char) + 1 + " ";
+    }
+  }
+
+  return result.trim();
+}
