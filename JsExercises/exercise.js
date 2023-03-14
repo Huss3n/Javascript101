56b800372482f3af4b67d29f7ee7e70f316b0bc9
Hussein <aisackhussein@gmail.com>
2023-03-14 20:08:50 +0300

    getSum

--- JsExercises/exercise.js
+++ JsExercises/exercise.js
@@ -287,3 +287,25 @@ function alphabetPosition(text) {
 
   return result.trim();
 }
+
+/*
+Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+Examples (a, b) --> output (explanation) */
+function getSum(a, b) {
+  if (a === b) {
+    return a;
+  } else {
+    let sum = 0;
+    const start = Math.min(a, b);
+    const end = Math.max(a, b);
+    for (let i = start; i <= end; i++) {
+      sum += i;
+    }
+    return sum;
+  }
+}
+
+console.log(getSum(2, 4));
