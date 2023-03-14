e738b32a623e563d4a0c019b1ec708b0d26de4a1
Hussein <aisackhussein@gmail.com>
2023-03-14 10:55:25 +0300

    Count vowels

--- JsExercises/exercise.js
+++ JsExercises/exercise.js
@@ -1,14 +1,28 @@
 // Write a function to reverse a number
 `use strict`;
 
-const reverse = (number) => {
-  var num = 0;
-  while (num != 0) {
-    num = num * 10;
-    num = num + (number % 10);
-    number = Math.trunc(number / 10); // remove the decimal places
+function reverse(number) {
+  var num = 0; // 0
+  while (number != 0) {
+    num = num * 10; // 0
+    num = num + (number % 10); // (0 + (123 % 10) = 3)
+    number = Math.trunc(number / 10); // (123 / 10) = 12
   }
   return num;
-};
-
+}
 console.log(reverse(123));
+console.log(reverse(456));
+console.log(reverse(987));
+
+// 2. Count number of Vowels in String
+function vowel(string) {
+  var num = 0;
+  string = string.toLowerCase();
+  for (var i = 0; i < string.length; i++) {
+    if (string.charAt(i) == "a" || string.charAt(i) == "e" || string.charAt(i) == "i" || string.charAt(i) == "o" || string.charAt(i) == "u") {
+      num++; //Increment vowel count
+    }
+  }
+  return num;
+}
+console.log(vowel("Academic Trial")); //6
