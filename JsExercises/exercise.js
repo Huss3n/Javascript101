4518a0149e84af8d51845a312b91c4a437b19b91
Hussein <aisackhussein@gmail.com>
2023-03-14 22:02:43 +0300

    move zeros

--- JsExercises/exercise.js
+++ JsExercises/exercise.js
@@ -514,3 +514,23 @@ function sumOfNthRow(n) {
 console.log(sumOfNthRow(1)); // Output: 1
 console.log(sumOfNthRow(2)); // Output: 8
 console.log(sumOfNthRow(3)); // Output: 24
+
+/**
+ Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+ */
+function moveZeros(arr) {
+  let nonZeros = [];
+  let zeros = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      zeros.push(arr[i]);
+    } else {
+      nonZeros.push(arr[i]);
+    }
+  }
+  return nonZeros.concat(zeros);
+}
+
+// Example usage
+console.log(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"])); // Output: [false,1,1,2,1,3,"a",0,0]
