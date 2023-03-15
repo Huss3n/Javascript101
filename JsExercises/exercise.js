18ae935b46790dc02a96abb9fd5afe119255e696
Hussein <aisackhussein@gmail.com>
2023-03-15 14:51:43 +0300

    Binomial Expansion

--- JsExercises/exercise.js
+++ JsExercises/exercise.js
@@ -1950,3 +1950,14 @@ assembler_interpreter(program)
 The above code would set register a to 5, increase its value by 1, calls the subroutine function, divide its value by 2, returns to the first call instruction, prepares the output of the program and then returns it with the end instruction. In this case, the output would be (5+1)/2 = 3.
 
  */
+
+/*
+Binomial Expansion
+DESCRIPTION:
+The purpose of this kata is to write a program that can do some algebra.
+
+Write a function expand that takes in an expression with a single, one character variable, and expands it. The expression is in the form (ax+b)^n where a and b are integers which may be positive or negative, x is any single character variable, and n is a natural number. If a = 1, no coefficient will be placed in front of the variable. If a = -1, a "-" will be placed in front of the variable.
+
+The expanded form should be returned as a string in the form ax^b+cx^d+ex^f... where a, c, and e are the coefficients of the term, x is the original one character variable that was passed in the original expression and b, d, and f, are the powers that x is being raised to in each term and are in decreasing order.
+
+If the coefficient of a term is zero, the term should not be included. If the coefficient of a term is one, the coefficient should not be included. If the coefficient of a term is -1, only the "-" should be included. If the power of the term is 0, only the coefficient should be included. If the power of the term is 1, the caret and power should be excluded. */
