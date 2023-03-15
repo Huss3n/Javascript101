f52e716fb6a6dd90d952794adac66f9fd80ae265
Hussein <aisackhussein@gmail.com>
2023-03-15 13:06:19 +0300

    Assembler interpreter

--- JsExercises/exercise.js
+++ JsExercises/exercise.js
@@ -1825,3 +1825,128 @@ Note : in C++ you are provided with two utility functions :
 std::string join(const std::string &sep, const std::vector<std::string> &to_join); // Returns the concatenation of all the strings in the vector, separated with sep 
 
 std::vector<std::string> split_lines(const std::string &to_split); // Splits a string, using separator '\n' */
+
+function extractShapes(diagram) {
+  const shapes = [];
+  const rows = diagram.trim().split("\n");
+  const width = rows[0].length;
+  const height = rows.length;
+  const visited = new Array(height).fill().map(() => new Array(width).fill(false));
+  const queue = [];
+
+  function isBorder(row, col) {
+    return row >= 0 && col >= 0 && row < height && col < width && (rows[row][col] === "+" || rows[row][col] === "-" || rows[row][col] === "|");
+  }
+
+  function bfs(startRow, startCol) {
+    const shape = [];
+    queue.push([startRow, startCol]);
+    while (queue.length > 0) {
+      const [row, col] = queue.shift();
+      if (visited[row][col]) continue;
+      visited[row][col] = true;
+      if (!isBorder(row, col)) return null;
+      shape.push([row, col]);
+      queue.push([row - 1, col], [row + 1, col], [row, col - 1], [row, col + 1]);
+    }
+    return shape;
+  }
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      if (visited[row][col]) continue;
+      if (rows[row][col] !== "+") continue;
+      const shape = bfs(row, col);
+      if (shape !== null) {
+        shapes.push(shape);
+      }
+    }
+  }
+
+  return shapes.map((shape) => {
+    const minRow = Math.min(...shape.map(([row, _]) => row));
+    const maxRow = Math.max(...shape.map(([row, _]) => row));
+    const minCol = Math.min(...shape.map(([_, col]) => col));
+    const maxCol = Math.max(...shape.map(([_, col]) => col));
+    const rows = [];
+    for (let row = minRow; row <= maxRow; row++) {
+      let line = "";
+      for (let col = minCol; col <= maxCol; col++) {
+        line += isBorder(row, col) ? diagram[row * (width + 1) + col] : " ";
+      }
+      rows.push(line.trimRight());
+    }
+    return rows.join("\n");
+  });
+}
+
+// const assert = require('assert');
+
+// const input = `+------------+
+// |            |
+// |            |
+// |            |
+// +------+-----+
+// |      |     |
+// |      |     |
+// +------+-----+`;
+// const expected = [
+//   '+------------+\n|            |\n|            |\n|            |\n+------------+',
+//   '+------+\n|      |\n|      |\n
+
+/*
+DESCRIPTION:
+This is the second part of this kata series. First part is here.
+
+We want to create an interpreter of assembler which will support the following instructions:
+
+mov x, y - copy y (either an integer or the value of a register) into register x.
+inc x - increase the content of register x by one.
+dec x - decrease the content of register x by one.
+add x, y - add the content of the register x with y (either an integer or the value of a register) and stores the result in x (i.e. register[x] += y).
+sub x, y - subtract y (either an integer or the value of a register) from the register x and stores the result in x (i.e. register[x] -= y).
+mul x, y - same with multiply (i.e. register[x] *= y).
+div x, y - same with integer division (i.e. register[x] /= y).
+label: - define a label position (label = identifier + ":", an identifier being a string that does not match any other command). Jump commands and call are aimed to these labels positions in the program.
+jmp lbl - jumps to the label lbl.
+cmp x, y - compares x (either an integer or the value of a register) and y (either an integer or the value of a register). The result is used in the conditional jumps (jne, je, jge, jg, jle and jl)
+jne lbl - jump to the label lbl if the values of the previous cmp command were not equal.
+je lbl - jump to the label lbl if the values of the previous cmp command were equal.
+jge lbl - jump to the label lbl if x was greater or equal than y in the previous cmp command.
+jg lbl - jump to the label lbl if x was greater than y in the previous cmp command.
+jle lbl - jump to the label lbl if x was less or equal than y in the previous cmp command.
+jl lbl - jump to the label lbl if x was less than y in the previous cmp command.
+call lbl - call to the subroutine identified by lbl. When a ret is found in a subroutine, the instruction pointer should return to the instruction next to this call command.
+ret - when a ret is found in a subroutine, the instruction pointer should return to the instruction that called the current function.
+msg 'Register: ', x - this instruction stores the output of the program. It may contain text strings (delimited by single quotes) and registers. The number of arguments isn't limited and will vary, depending on the program.
+end - this instruction indicates that the program ends correctly, so the stored output is returned (if the program terminates without this instruction it should return the default output: see below).
+; comment - comments should not be taken in consideration during the execution of the program.
+
+Output format:
+The normal output format is a string (returned with the end command). For Scala and Rust programming languages it should be incapsulated into Option.
+
+If the program does finish itself without using an end instruction, the default return value is:
+
+-1 (as an integer)
+
+Input format:
+The function/method will take as input a multiline string of instructions, delimited with EOL characters. Please, note that the instructions may also have indentation for readability purposes.
+
+For example:
+
+program = """
+; My first program
+mov  a, 5
+inc  a
+call function
+msg  '(5+1)/2 = ', a    ; output message
+end
+
+function:
+    div  a, 2
+    ret
+"""
+assembler_interpreter(program)
+The above code would set register a to 5, increase its value by 1, calls the subroutine function, divide its value by 2, returns to the first call instruction, prepares the output of the program and then returns it with the end instruction. In this case, the output would be (5+1)/2 = 3.
+
+ */
