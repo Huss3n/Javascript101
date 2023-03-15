8caeb0eafb03ab5859ecc073b9555c34373c6fd5
Hussein <aisackhussein@gmail.com>
2023-03-15 07:19:27 +0300

    break the pieces

--- JsExercises/exercise.js
+++ JsExercises/exercise.js
@@ -1742,3 +1742,86 @@ Constraints: 2 ≤ b ≤ 10000.
 
 The number of inner points with integer coordinates.
  */
+
+function rectangleRotation(a, b) {
+  const sideA = Math.floor(a * Math.sqrt(2));
+  const sideB = Math.floor(b * Math.sqrt(2));
+  const startX = -sideA;
+  const endX = sideA;
+  const startY = -sideB;
+  const endY = sideB;
+  let count = 0;
+
+  for (let x = startX; x <= endX; x++) {
+    for (let y = startY; y <= endY; y++) {
+      const rotatedX = (x * Math.sqrt(2) - y * Math.sqrt(2)) / 2;
+      const rotatedY = (x * Math.sqrt(2) + y * Math.sqrt(2)) / 2;
+
+      if (Math.abs(rotatedX) <= a / 2 && Math.abs(rotatedY) <= b / 2) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
+const a = 6;
+const b = 4;
+const result = rectangleRotation(a, b);
+
+/*
+
+Break the pieces
+You are given a ASCII diagram, comprised of minus signs -, plus signs +, vertical bars | and whitespaces   . Your task is to write a function which breaks the diagram in the minimal pieces it is made of.
+
+For example, if the input for your function is this diagram:
+
++------------+
+|            |
+|            |
+|            |
++------+-----+
+|      |     |
+|      |     |
++------+-----+
+the returned value should be the list of:
+
++------------+
+|            |
+|            |
+|            |
++------------+
+(note how it lost a + sign in the extraction)
+
+as well as
+
++------+
+|      |
+|      |
++------+
+and
+
++-----+
+|     |
+|     |
++-----+
+The diagram is given as an ordinary multiline string. There are no borders touching each others.
+
+The pieces should not have trailing spaces at the end of the lines. However, it could have leading spaces if the figure is not a rectangle. For instance:
+
+    +---+
+    |   |
++---+   |
+|       |
++-------+
+However, it is not allowed to use more leading spaces than necessary. It is to say, the first character of some of the lines should be different than a space.
+
+Finally, note that only the explicitly closed pieces are considered. Spaces "outside" of the shape are part of the background . Therefore the diagram above has a single piece.
+
+Have fun!
+
+Note : in C++ you are provided with two utility functions :
+
+std::string join(const std::string &sep, const std::vector<std::string> &to_join); // Returns the concatenation of all the strings in the vector, separated with sep 
+
+std::vector<std::string> split_lines(const std::string &to_split); // Splits a string, using separator '\n' */
