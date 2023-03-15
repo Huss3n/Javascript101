068f8b219b0e1bac4a342f28f49a3405a3fa4bb2
Hussein <aisackhussein@gmail.com>
2023-03-15 06:14:29 +0300

    runners up soln

--- JsExercises/exercise.js
+++ JsExercises/exercise.js
@@ -1511,3 +1511,17 @@ There are  students in this class whose names and grades are assembled to build 
 python students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
 
 The lowest grade of  belongs to Tina. The second lowest grade of  belongs to both Harry and Berry, so we order their names alphabetically and print each name on a new line. */
+
+function findRunnerUpScore(n, arr) {
+  // Convert the input array to a Set to remove duplicates and sort it in descending order
+  const uniqueScores = new Set(arr);
+  const sortedScores = Array.from(uniqueScores).sort((a, b) => b - a);
+
+  // Return the second largest score
+  return sortedScores[1];
+}
+
+const n = parseInt(readLine());
+const arr = readLine().split(" ").map(Number);
+
+console.log(findRunnerUpScore(n, arr));
