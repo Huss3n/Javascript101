db3659bca8b1cf8f7788f457be36c069f9f203d5
Hussein <aisackhussein@gmail.com>
2023-03-16 21:57:12 +0300

    Odd soln

--- JsExercises/exercise.js
+++ JsExercises/exercise.js
@@ -2540,6 +2540,10 @@ Examples
 [1,1,2] should return 2, because it occurs 1 time (which is odd).
 [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
-
-
  */
+
+function findOdd(arr) {
+  let result = 0;
+  arr.forEach((num) => (result ^= num));
+  return result;
+}
