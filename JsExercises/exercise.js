5cd3fd7b171eac95811ed4c006f7b5317f8d3a98
Hussein <aisackhussein@gmail.com>
2023-03-16 21:55:05 +0300

    Reverse string

--- JsExercises/exercise.js
+++ JsExercises/exercise.js
@@ -2522,3 +2522,8 @@ Complete the solution so that it reverses the string passed into it.
 
 'world'  =>  'dlrow'
 'word'   =>  'drow' */
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+console.log(reverseString("world")); // 'dlrow'
+console.log(reverseString("word")); // 'drow'
