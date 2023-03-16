3eba0607c79697728c5b822ed040923896ca614a
Hussein <aisackhussein@gmail.com>
2023-03-16 21:40:10 +0300

    Detect paragm

--- JsExercises/exercise.js
+++ JsExercises/exercise.js
@@ -2409,3 +2409,8 @@ function calculate(expression) {
   // Call the recursive evaluate function to get the result
   return evaluate();
 }
+
+/*
+A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation. */
