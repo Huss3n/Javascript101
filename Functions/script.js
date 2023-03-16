3a4bb2a7b84533f5d4970aaa70e2db6196c85b04
Hussein <aisackhussein@gmail.com>
2023-03-16 23:18:50 +0300

    Two to One

--- Functions/script.js
+++ Functions/script.js
@@ -320,3 +320,15 @@ const h = function () {
 // h is a closure of f
 h();
 f();
+
+/*
+Two to One
+Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz" */
