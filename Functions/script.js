c268eab04749842e36040fe7cfcf02f6e76880b4
Hussein <aisackhussein@gmail.com>
2023-05-05 22:24:32 +0300

    Scope chain

--- Functions/script.js
+++ Functions/script.js
@@ -519,3 +519,14 @@ function a() {
     }
   }
 }
+
+// scope chain
+function outside() {
+  const x = 5;
+  function inside(x) {
+    return x * 2;
+  }
+  return inside;
+}
+
+console.log(outside()(10)); // 20 (instead of 10)
