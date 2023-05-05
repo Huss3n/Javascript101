14b538f107e089723096f677a758d2fd670baec3
Hussein <aisackhussein@gmail.com>
2023-05-05 22:05:53 +0300

    recursive function

--- Functions/script.js
+++ Functions/script.js
@@ -486,3 +486,13 @@ function getScore() {
 }
 
 console.log(getScore()); // "Chamakh scored 5"
+
+function loop(x) {
+  // "x >= 10" is the exit condition (equivalent to "!(x < 10)")
+  if (x >= 10) {
+    return;
+  }
+  // do stuff
+  loop(x + 1); // the recursive call
+}
+loop(0);
