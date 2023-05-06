e7b971b0bf72b661268b13f09e68ac44a759a4e1
Hussein <aisackhussein@gmail.com>
2023-05-06 23:33:26 +0300

    Queues

--- AsyncAwait/index.ts
+++ AsyncAwait/index.ts
@@ -83,3 +83,16 @@ doSomething(() => {
   value = 2;
 });
 console.log(value); // 1 or 2?
+
+const promise = new Promise((resolve, reject) => {
+  console.log("Promise callback");
+  resolve();
+}).then((result) => {
+  console.log("Promise callback (.then)");
+});
+
+setTimeout(() => {
+  console.log("event-loop cycle: Promise (fulfilled)", promise);
+}, 0);
+
+console.log("Promise (pending)", promise);
