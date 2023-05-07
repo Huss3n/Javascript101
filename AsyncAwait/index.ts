e37671965384c726144d85327c0de8fafef91297
Hussein <aisackhussein@gmail.com>
2023-05-07 22:04:37 +0300

    ty

--- AsyncAwait/index.ts
+++ AsyncAwait/index.ts
@@ -167,3 +167,15 @@ function doOperation() {
 }
 
 doOperation();
+
+type One = { p: string };
+interface Two {
+  p: string;
+}
+class Three {
+  p = "Hello";
+}
+
+let x: One = { p: "hi" };
+let two: Two = x;
+two = new Three();
