539f43d0298bd323b5348a7270179a6767075622
Hussein <aisackhussein@gmail.com>
2023-05-08 21:35:54 +0300

    tupples

--- AsyncAwait/index.ts
+++ AsyncAwait/index.ts
@@ -198,3 +198,10 @@ numbers.push(4); // no error
 // comment line below out to see the successful assignment
 numbers.push("2"); // Error: Argument of type 'string' is not assignable to parameter of type 'number'.
 let head: number = numbers[0]; // no error
+
+// tupples
+// define our tuple
+let ourTuple: [number, boolean, string];
+
+// initialize correctly
+ourTuple = [5, false, "Coding God was here"];
