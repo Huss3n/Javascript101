026204de11d63cd655258ca5aaf865b562ce28a9
Hussein <aisackhussein@gmail.com>
2023-05-08 21:37:26 +0300

    readonly tupples

--- AsyncAwait/index.ts
+++ AsyncAwait/index.ts
@@ -211,3 +211,11 @@ let ourTuple: [number, boolean, string];
 
 // initialized incorrectly which throws an error
 ourTuple = [false, "Coding God was mistaken", 5];
+
+// define our tuple
+let ourTuple: [number, boolean, string];
+// initialize correctly
+ourTuple = [5, false, "Coding God was here"];
+// We have no type safety in our tuple for indexes 3+
+ourTuple.push("Something new and wrong");
+console.log(ourTuple);
