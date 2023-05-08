98cb797826754bb08b5c7689ac23d095c9e20ed8
Hussein <aisackhussein@gmail.com>
2023-05-08 21:40:03 +0300

    enums

--- AsyncAwait/index.ts
+++ AsyncAwait/index.ts
@@ -240,3 +240,16 @@ const car: { type: string; mileage: number } = {
   type: "Toyota",
 };
 car.mileage = 2000;
+
+// enums
+enum CardinalDirections {
+  North,
+  East,
+  South,
+  West,
+}
+let currentDirection = CardinalDirections.North;
+// logs 0
+console.log(currentDirection);
+// throws error as 'North' is not a valid enum
+currentDirection = "North"; // Error: "North" is not assignable to type 'CardinalDirections'.
