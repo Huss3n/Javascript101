7dd144c83a25bbae0deac7ebcfbcd9610b75222f
Hussein <aisackhussein@gmail.com>
2023-05-08 21:39:32 +0300

    optional properties

--- AsyncAwait/index.ts
+++ AsyncAwait/index.ts
@@ -233,3 +233,10 @@ const car = {
 };
 car.type = "Ford"; // no error
 car.type = 2; // Error: Type 'number' is not assignable to type 'string'.
+
+// optional properties
+const car: { type: string; mileage: number } = {
+  // Error: Property 'mileage' is missing in type '{ type: string; }' but required in type '{ type: string; mileage: number; }'.
+  type: "Toyota",
+};
+car.mileage = 2000;
