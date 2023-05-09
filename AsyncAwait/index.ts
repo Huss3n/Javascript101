c491d3d6e560c5b9941791f03c39515e1f222ad8
Hussein <aisackhussein@gmail.com>
2023-05-09 22:56:24 +0300

    Type guards

--- AsyncAwait/index.ts
+++ AsyncAwait/index.ts
@@ -312,3 +312,18 @@ class Person {
     return `${this.firstName} ${this.lastName}`;
   }
 }
+
+// type guards
+type alphanumeric = string | number;
+
+function add(a: alphanumeric, b: alphanumeric) {
+  if (typeof a === "number" && typeof b === "number") {
+    return a + b;
+  }
+
+  if (typeof a === "string" && typeof b === "string") {
+    return a.concat(b);
+  }
+
+  throw new Error("Invalid arguments. Both arguments must be either numbers or strings.");
+}
