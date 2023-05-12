edc48de014d4f28519854d4c5846e40be2d84741
Hussein <aisackhussein@gmail.com>
2023-05-12 15:43:35 +0300

    Destructuring

--- Fundamentals_1/objects/script.ts
+++ Fundamentals_1/objects/script.ts
@@ -18,3 +18,13 @@ console.log(Object.values(obj)); // [ 'hussein', [ 'abdulahi', 'wyse', 'muktar' 
 
 // the keys method -> removes the value and gets the keys
 console.log(Object.keys(obj)); //[ 'name', 'friends', 'phone' ]
+
+// destructurinng -> like a spread opearator but for easy adds and assigns
+let p, r, others;
+[p, r, ...others] = [12, 34, 23, 34324, 54, 24, 43, 24, 34];
+
+console.log(others); // output
+//     23, 34324, 54,
+//     24,    43, 24,
+//     34
+//   ]
