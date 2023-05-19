8beb1a1309b4893bd67cc3779cd6d2c524cd67bc
Hussein <aisackhussein@gmail.com>
2023-05-19 23:11:07 +0300

    App js

--- Project/ThePigGame/app.js
+++ Project/ThePigGame/app.js
@@ -142,3 +142,15 @@ btnNew.addEventListener("click", init);
 
 import React from "react";
 import ReactDOM from "react-dom";
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <h1>Heading one</h1>
+      </div>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
