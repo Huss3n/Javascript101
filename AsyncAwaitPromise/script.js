ed281c5f5c735d4dec0b1435319d8d91d0e146ef
Hussein <aisackhussein@gmail.com>
2023-05-25 11:39:51 +0300

    Call back hell review

--- AsyncAwaitPromise/script.js
+++ AsyncAwaitPromise/script.js
@@ -4,15 +4,17 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
-const request = new XMLHttpRequest();
-request.open("GET", "https://restcountries.com/v3.1/name/canada");
-request.send();
 
-request.addEventListener("load", function () {
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
+function getCountry(country) {
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
 
-  const html = `
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `
   <article class="country">
   <img class="country__img" src="${data.flags.svg}" />
   <div class="country__data">
@@ -25,6 +27,28 @@ request.addEventListener("load", function () {
 </article>
   `;
 
-  countriesContainer.insertAdjacentHTML("beforeend", html);
-  countriesContainer.style.opacity = 1;
-});
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+  });
+}
+
+getCountry("canada");
+
+// call back hell example
+// setTimeout(() => {
+//   console.log("1 Second passed");
+//   setTimeout(() => {
+//     console.log("2 Seconds passed");
+//     setTimeout(() => {
+//       console.log("3 Seconds passed");
+//       setTimeout(() => {
+//         console.log("4 Seconds passed");
+//       }, 1000);
+//       setTimeout(() => {
+//         setTimeout(() => {
+//           console.log("6 Seconds passed");
+//         }, 1000);
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
